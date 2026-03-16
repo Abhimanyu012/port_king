da728ba717446aa468ff2c1eef0df4c3638f746c
abhimanyu012 <abhimanyucode2021@gmail.com>
2026-03-16 23:04:32 +0530

    fix: correct server log message format and clean up health check endpoint

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import app from "./app.js";
 const PORT = 5000;
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "ok" });
+  // res.send(`<button>button</button>`)
 });
 
 app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
